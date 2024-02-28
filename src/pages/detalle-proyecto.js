efa3eb502ecbb8abdf80733536568bfa6d28527e
DiegoAle6 <diegoalemanmena@gmail.com>
2024-02-28 12:05:40 -0500

    ddtails

--- src/pages/detalle-proyecto.js
+++ src/pages/detalle-proyecto.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import '../App.css'; // Asegúrate de importar tu archivo de estilos CSS
+
 
 const ProjectDetailsPage = () => {
   // Estados para los campos del formulario
@@ -20,82 +22,103 @@ const ProjectDetailsPage = () => {
     // Lógica de búsqueda aquí
     console.log('Buscar estudiante con matrícula:', studentId);
   };
+  // Función para manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de envío aquí
+    console.log('Formulario enviado', {
+      company,
+      advisor,
+      projectTitle,
+      career,
+      startDate,
+      endDate,
+      objectives,
+      problemStatement,
+      justification,
+      activities
+    });
+  };
+
+  // Función para manejar la eliminación
+  const handleDelete = () => {
+    // Lógica de eliminación aquí
+    console.log('Proyecto eliminado');
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-3xl">
+
+
+    <div className="centered-box">
+
+    <div className="container mx-auto p-8">
+    
+
+    
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
-          <h1 className="text-4xl font-bold mb-6 uppercase text-center text-blue-900">DETALLES DEL PROYECTO</h1>
-          <div className="mb-6">
-            <div className="mb-4">
-              <label htmlFor="student-id" className="block text-subtitle text-sm font-bold mb-2">
-                Ingresa la Matrícula del Estudiante Colaborador
-              </label>
-              <div className="flex">
-                <input
-                  type="text"
-                  id="student-id"
-                  value={studentId}
-                  onChange={e => setStudentId(e.target.value)}
-                  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full mr-2"
-                />
-                <button
-                  type="button"
-                  onClick={handleSearch}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  BUSCAR
-                </button>
-              </div>
-            </div>
-            <h2 className="text-2xl font-bold mb-4 text-blue-900">Detalles Del Proyecto:</h2>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <label htmlFor="company-name" className="block text-subtitle text-sm font-bold mb-2">Empresa</label>
-              <input type="text" id="company-name" value={company} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="advisor-name" className="block text-subtitle text-sm font-bold mb-2">Asesor</label>
-              <input type="text" id="advisor-name" value={advisor} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="project-title" className="block text-subtitle text-sm font-bold mb-2">Título del Proyecto</label>
-              <input type="text" id="project-title" value={projectTitle} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="career" className="block text-subtitle text-sm font-bold mb-2">Carrera</label>
-              <input type="text" id="career" value={career} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="start-date" className="block text-subtitle text-sm font-bold mb-2">Inicio</label>
-              <input type="date" id="start-date" value={startDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="end-date" className="block text-subtitle text-sm font-bold mb-2">Fin</label>
-              <input type="date" id="end-date" value={endDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="objectives" className="block text-subtitle text-sm font-bold mb-2">1. Objetivo General del Proyecto.</label>
-              <textarea id="objectives" value={objectives} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="problem-statement" className="block text-subtitle text-sm font-bold mb-2">2. Planteamiento del Problema:</label>
-              <textarea id="problem-statement" value={problemStatement} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="justification" className="block text-subtitle text-sm font-bold mb-2">3. Justificación</label>
-              <textarea id="justification" value={justification} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
-            </div>
-            <div>
-              <label htmlFor="activities" className="block text-subtitle text-sm font-bold mb-2">4. Actividades a realizar</label>
-              <textarea id="activities" value={activities} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" readOnly />
+          <h1 className="block text-4xl font-bold mb-2  uppercase text-blue-900">EDICIÓN DEL ANTE-PROYECTO</h1>
+          <div className="mb-4">
+            <label htmlFor="student-id" className="block text-subtitle text-sm font-bold mb-2">
+              Ingresa la Matricula del Estudiante Colaborador
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                id="student-id"
+                value={studentId}
+                onChange={e => setStudentId(e.target.value)}
+                className="shadow appearance-none border rounded py-2 px-3 text-gray-700 
+                           leading-tight focus:outline-none focus:shadow-outline w-1/2 mr-2"
+              />
+              <button
+                type="button"
+                onClick={handleSearch}
+                className="save-button"
+              >
+                BUSCAR
+              </button>
             </div>
           </div>
+          <h1 className="block text-2xl font-bold mb-2 text-center text-blue-900">Detalles Del Proyecto:</h1>
+
+          <label htmlFor="company-name" className="block text-subtitle text-sm font-bold mb-2">Empresa</label>
+          <input type="text" id="company-name" value={company} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="advisor-name" className="block text-subtitle text-sm font-bold mb-2">Asesor</label>
+          <input type="text" id="advisor-name" value={advisor} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="project-title" className="block text-subtitle text-sm font-bold mb-2">Título del Proyecto</label>
+          <input type="text" id="project-title" value={projectTitle} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="career" className="block text-subtitle text-sm font-bold mb-2">Carrera</label>
+          <input type="text" id="career" value={career} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="start-date" className="block text-subtitle text-sm font-bold mb-2">Inicio</label>
+          <input type="date" id="start-date" value={startDate} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="end-date" className="block text-subtitle text-sm font-bold mb-2">Fin</label>
+          <input type="date" id="end-date" value={endDate} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+
+          <label htmlFor="objectives" className="block text-subtitle text-sm font-bold mb-2">1. Objetivo General del Proyecto.</label>
+          <textarea id="objectives" value={objectives} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+
+          <label htmlFor="problem-statement" className="block text-subtitle text-sm font-bold mb-2">2. Planteamiento del Problema:</label>
+          <textarea id="problem-statement" value={problemStatement} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+          
+          <label htmlFor="justification" className="block text-subtitle text-sm font-bold mb-2">3. Justificación</label>
+          <textarea id="justification" value={justification} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+
+          <label htmlFor="activities" className="block text-subtitle text-sm font-bold mb-2">4. Actividades a realizar</label>
+          <textarea id="activities" value={activities} readOnly className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+
         </div>
-      </div>
+      </form>
     </div>
+        </div>
+
   );
 };
+
 
 export default ProjectDetailsPage;
