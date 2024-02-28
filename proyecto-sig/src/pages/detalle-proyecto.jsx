e3de235a9bb2e107e419fbf6e5820dbb70da67f8
GussGtz <gutierrezgustavocanul@gmail.com>
2024-02-27 23:12:03 -0500

    mejore estilos

--- proyecto-sig/src/pages/detalle-proyecto.jsx
+++ proyecto-sig/src/pages/detalle-proyecto.jsx
@@ -50,60 +50,63 @@ const FormPage = () => {
         <div className="mb-4">
           <h1 className="block text-gray-700 text-lg font-bold mb-2">Edición del ante-proyecto</h1>
           <div className="mb-4">
-            <label htmlFor="student-id" className="block text-gray-700 text-sm font-bold mb-2">
+            <label htmlFor="student-id" className="block text-subtitle text-sm font-bold mb-2">
               Ingresa la Matricula del Estudiante Colaborador
             </label>
             <div className="flex">
-              <input
-                type="text"
-                id="student-id"
-                value={studentId}
-                onChange={e => setStudentId(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-              <button
-                type="button"
-                onClick={handleSearch}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                BUSCAR
-              </button>
-            </div>
+  <input
+    type="text"
+    id="student-id"
+    value={studentId}
+    onChange={e => setStudentId(e.target.value)}
+    className="shadow appearance-none border rounded py-2 px-3 text-gray-700 
+               leading-tight focus:outline-none focus:shadow-outline w-1/2 mr-2"
+  />
+
+  <button
+    type="button"
+    onClick={handleSearch}
+    className="save-button"
+  >
+    BUSCAR
+  </button>
+</div>
+
           </div>
 
-          <label htmlFor="company-name" className="block text-gray-700 text-sm font-bold mb-2">Empresa</label>
+          <label htmlFor="company-name" className="block text-subtitle text-sm font-bold mb-2">Empresa</label>
           <input type="text" id="company-name" value={company} onChange={e => setCompany(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="advisor-name" className="block text-gray-700 text-sm font-bold mb-2">Asesor</label>
+          <label htmlFor="advisor-name" className="block text-subtitle text-sm font-bold mb-2">Asesor</label>
           <input type="text" id="advisor-name" value={advisor} onChange={e => setAdvisor(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="project-title" className="block text-gray-700 text-sm font-bold mb-2">Título del Proyecto</label>
+          <label htmlFor="project-title" className="block text-subtitle text-sm font-bold mb-2">Título del Proyecto</label>
           <input type="text" id="project-title" value={projectTitle} onChange={e => setProjectTitle(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="career" className="block text-gray-700 text-sm font-bold mb-2">Carrera</label>
+          <label htmlFor="career" className="block text-subtitle text-sm font-bold mb-2">Carrera</label>
           <input type="text" id="career" value={career} onChange={e => setCareer(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="start-date" className="block text-gray-700 text-sm font-bold mb-2">Inicio</label>
+          <label htmlFor="start-date" className="block text-subtitle text-sm font-bold mb-2">Inicio</label>
           <input type="date" id="start-date" value={startDate} onChange={e => setStartDate(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="end-date" className="block text-gray-700 text-sm font-bold mb-2">Fin</label>
+          <label htmlFor="end-date" className="block text-subtitle text-sm font-bold mb-2">Fin</label>
           <input type="date" id="end-date" value={endDate} onChange={e => setEndDate(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
 
-          <label htmlFor="objectives" className="block text-gray-700 text-sm font-bold mb-2">1. Objetivo General del Proyecto.</label>
+          <label htmlFor="objectives" className="block text-subtitle text-sm font-bold mb-2">1. Objetivo General del Proyecto.</label>
           <textarea id="objectives" value={objectives} onChange={e => setObjectives(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
 
-          <label htmlFor="problem-statement" className="block text-gray-700 text-sm font-bold mb-2">2. Planteamiento del Problema:</label>
+          <label htmlFor="problem-statement" className="block text-subtitle text-sm font-bold mb-2">2. Planteamiento del Problema:</label>
           <textarea id="problem-statement" value={problemStatement} onChange={e => setProblemStatement(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
           
-          <label htmlFor="justification" className="block text-gray-700 text-sm font-bold mb-2">3. Justificación</label>
+          <label htmlFor="justification" className="block text-subtitle text-sm font-bold mb-2">3. Justificación</label>
           <textarea id="justification" value={justification} onChange={e => setJustification(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
 
-          <label htmlFor="activities" className="block text-gray-700 text-sm font-bold mb-2">4. Actividades a realizar</label>
+          <label htmlFor="activities" className="block text-subtitle text-sm font-bold mb-2">4. Actividades a realizar</label>
           <textarea id="activities" value={activities} onChange={e => setActivities(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
 
           <div className="flex items-center justify-between mt-4">
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Guardar</button>
-            <button type="button" onClick={handleDelete} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Eliminar</button>
+            <button type="submit" className="save-button2">Guardar</button>
+            <button type="button" onClick={handleDelete} className="delete-button">Eliminar</button>
           </div>
         </div>
       </form>
